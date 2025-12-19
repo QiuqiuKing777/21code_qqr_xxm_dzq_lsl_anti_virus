c6c3087672b64c7b09541ff9146194a19af4d67f
Zhongxiu Yang <2432843806@qq.com>
2025-12-19 19:12:25 +0800

    Modify comment for SQLALCHEMY_DATABASE_URI
    Updated SQLALCHEMY_DATABASE_URI comment to include MySQL password instructions.

--- vuln_backend/src/config.py
+++ vuln_backend/src/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    # 连接串
+     # 连接串 下面这一行要根据您的mysql密码来填写：SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:您的数据库密码@localhost:3306/nvd_database'
     SQLALCHEMY_DATABASE_URI = 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PORT = 3000
